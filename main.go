c0528459e5ad11dfe613a756d49213af0c1258f8
ricnish <ricnishimura@gmail.com>
2024-01-24 16:53:18 -0300

    Initial load

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func cmdRead() *cobra.Command {
+	var srcpath string
+
+	var cmdRead = &cobra.Command{
+		Use:   "read -p [resource path]",
+		Short: "Reads the version from resource file",
+		Run: func(cmd *cobra.Command, args []string) {
+			if srcpath == "" {
+				fmt.Println("The resource file path is missing")
+				return
+			}
+
+			ver := readVersion(srcpath)
+			if ver == "" {
+				fmt.Printf("No version string was found in the file %s\n", srcpath)
+			} else {
+				fmt.Println(ver)
+			}
+		},
+	}
+
+	cmdRead.Flags().StringVarP(&srcpath, "path", "p", "", "The resource file path")
+
+	return cmdRead
+}
+
+func cmdChange() *cobra.Command {
+
+	var srcpath, rcversion string
+
+	var cmdWrite = &cobra.Command{
+		Use:   "change -p [resource path] -v [version]",
+		Short: "Change the version in the resource file",
+		Run: func(cmd *cobra.Command, args []string) {
+			if srcpath == "" {
+				fmt.Println("The resource path is missing")
+				return
+			}
+
+			if rcversion == "" {
+				fmt.Println("The version parameter is missing")
+				return
+			}
+
+			count := changeVersion(srcpath, rcversion)
+			if count > 0 {
+				fmt.Printf("%d Updated file(s)\n", count)
+			} else {
+				fmt.Println("No file was updated")
+			}
+		},
+	}
+
+	cmdWrite.Flags().StringVarP(&srcpath, "path", "p", "", "The resource path")
+	cmdWrite.Flags().StringVarP(&rcversion, "version", "v", "", "The version to be updated")
+
+	return cmdWrite
+}
+
+func main() {
+
+	var cmdRoot = &cobra.Command{
+		Use:  "rcversion [command] [-p filepath/directory] [-v version]",
+		Long: "Read and change the version in MSVC resouce file (.rc).\n*The file must be enconded in ANSI or UTF-8",
+	}
+
+	cmdRoot.AddCommand(cmdRead())
+	cmdRoot.AddCommand(cmdChange())
+
+	cmdRoot.Execute()
+
+}
---
+++ rcfile.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+func replaceVersion(line string, version string) string {
+	versionComma := strings.ReplaceAll(version, ".", ",")
+	if strings.Index(line, " FILEVERSION ") == 0 {
+		return " FILEVERSION " + versionComma
+	}
+
+	if strings.Index(line, " PRODUCTVERSION ") == 0 {
+		return " PRODUCTVERSION " + versionComma
+	}
+
+	if strings.Contains(line, "VALUE \"FileVersion\"") {
+		return "            VALUE \"FileVersion\", \"" + version + "\""
+	}
+
+	if strings.Contains(line, "VALUE \"ProductVersion\"") {
+		return "            VALUE \"ProductVersion\", \"" + version + "\""
+	}
+
+	return line
+}
+
+func readVersion(rcpath string) string {
+	file, err := os.Open(rcpath)
+	if err != nil {
+		fmt.Printf("Não foi possivel abrir o arquivo: %s\n", rcpath)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "VALUE \"FileVersion\"") {
+			strs := strings.Split(line, ", ")
+			str := strs[1]
+			return strings.Trim(str, "\"")
+		}
+	}
+
+	return ""
+}
+
+func changeFileVersion(rcpath string, version string) bool {
+	if path.Ext(rcpath) != ".rc" {
+		fmt.Printf("Is not a .rc file %s\n", rcpath)
+		return false
+	}
+
+	file, err := os.Open(rcpath)
+	if err != nil {
+		fmt.Printf("Could not open the file %s\n", rcpath)
+		return false
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	file.Close()
+
+	tmpfile := strings.Replace(rcpath, ".rc", ".rcbk", 1)
+
+	newfile, err := os.Create(tmpfile)
+
+	if err != nil {
+		fmt.Printf("Could not create the file %s\n", tmpfile)
+		return false
+	}
+
+	w := bufio.NewWriter(newfile)
+	for _, line := range lines {
+		// Process each line for version change
+		newline := replaceVersion(line, version)
+		// Add line with CR LF ending
+		fmt.Fprintf(w, "%s\r\n", newline)
+	}
+	w.Flush()
+	newfile.Close()
+
+	err = os.Remove(rcpath)
+	if err != nil {
+		fmt.Printf("Could not delete the original file: %s\n", rcpath)
+		return false
+	}
+
+	err = os.Rename(tmpfile, rcpath)
+	if err != nil {
+		fmt.Printf("Could not rename the new file: %s\n", tmpfile)
+		return false
+	}
+	return true
+}
+
+func changeVersion(rcpath string, version string) int {
+	var count int = 0
+
+	fileinfo, err := os.Stat(rcpath)
+	if err != nil {
+		fmt.Printf("Could not find the path %s\n", rcpath)
+		return count
+	}
+
+	if fileinfo.IsDir() {
+		// If path is a folder, find resource files recursively
+		err = filepath.Walk(rcpath, func(walkpath string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Printf("Error reading path %s", walkpath)
+				return nil
+			}
+
+			if path.Ext(info.Name()) == ".rc" {
+				if changeFileVersion(walkpath, version) {
+					fmt.Println(walkpath)
+					count++
+				}
+			}
+			return nil
+		})
+
+		if err != nil {
+			fmt.Printf("Error walking the path %s", rcpath)
+		}
+
+		return count
+	} else {
+		// Just change a single file
+		if changeFileVersion(rcpath, version) {
+			count++
+		}
+	}
+
+	return count
+}
